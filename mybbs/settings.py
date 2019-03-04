623cdaf165f38a9746f2dc6a776bdca1d59bc1b6
ruanxw <411235314@qq.com>
2019-03-04 22:17:37 +0800

    modify static2 dir to heroku

--- mybbs/settings.py
+++ mybbs/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Heroku设置
 if os.getcwd() == '/app':
     import dj_database_url
@@ -29,9 +34,6 @@ if os.getcwd() == '/app':
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
