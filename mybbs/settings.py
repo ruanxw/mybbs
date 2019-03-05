dae03e71167e743d6a5b1104a4999c241b6ab53d
ruanxw <411235314@qq.com>
2019-03-05 20:59:56 +0800

    修改heroku的debug为false时的static配置

--- mybbs/settings.py
+++ mybbs/settings.py
@@ -164,7 +164,7 @@ if os.getcwd() == '/app':
     # 静态资产配置
     #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATIC_ROOT = ''
+    #STATIC_ROOT = 'static'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
--- mybbs/urls.py
+++ mybbs/urls.py
@@ -35,3 +35,7 @@ urlpatterns = [
 ]
 if os.getcwd() != '/app':
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG is False:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
