d504ebb741f5d3bb684bd1678bdf6a878cf12191
ruanxw <411235314@qq.com>
2019-03-05 20:43:16 +0800

    按日期归档文章

--- blog/templatetags/my_tags.py
+++ blog/templatetags/my_tags.py
@@ -5,6 +5,20 @@ from django.db.models import Count
 register = template.Library()
 
 
+def month_list(user):
+    articles = models.Article.objects.filter(user=user).all()
+    year_month = set()   #设置集合，无重复元素
+    for a in articles:
+        year_month.add((a.create_time.year,a.create_time.month))  #把每篇文章的年、月以元组形式添加到集合中
+    counter = {}.fromkeys(year_month,0)  #以元组作为key，初始化字典
+    for a in articles:
+        counter[(a.create_time.year,a.create_time.month)]+=1  # 按年月统计文章数目
+    year_month_number = []  #初始化列表
+    for key in counter:
+        year_month_number.append([key[0],key[1],counter[key]])  # 把字典转化为（年，月，数目）元组为元素的列表
+    year_month_number.sort(reverse=True)  # 排序
+    return year_month_number  #返回字典context
+
 @register.inclusion_tag("left_menu.html")
 def get_left_menu(username):
     user = models.UserInfo.objects.filter(username=username).first()
@@ -14,12 +28,14 @@ def get_left_menu(username):
     # 查文章标签及对应的文章数
     tag_list = models.Tag.objects.filter(blog=blog).annotate(c=Count("article")).values("title", "c").exclude(title='default')
 
-    # 按日期归档
+    # 按日期归档 #mysql
     # archive_list = models.Article.objects.filter(user=user).extra(
     #     select={"archive_ym": "date_format(create_time,'%%Y-%%m')"}
     # ).values("archive_ym").annotate(c=Count("nid")).values("archive_ym", "c")
-    archive_list = models.Article.objects.raw(
-        'select nid, count(nid) as num,strftime("%Y-%m",create_time) as ctime from blog_article group by strftime("%Y-%m",create_time) order by create_time desc' )
+    # 按日期归档 #sqlite3
+    # archive_list = models.Article.objects.filter(user=user).raw(
+    #     'select nid, count(nid) as num,strftime("%Y-%m",create_time) as ctime from blog_article group by strftime("%Y-%m",create_time) order by create_time desc' )
+    archive_list = month_list(user)
     return {
         "blog_site": blog.site,
         "category_list" :category_list,
--- mybbs/settings.py
+++ mybbs/settings.py
@@ -131,10 +131,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-STATIC_ROOT = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+#"上传heroku要打开STATIC_ROOT，关闭STATICFILES_DIRS，本地使用则相反"
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # 告诉django项目使用那张表做认证
 AUTH_USER_MODEL = 'blog.UserInfo'
